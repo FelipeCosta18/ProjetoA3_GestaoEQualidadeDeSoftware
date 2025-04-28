49455fba8b4fa4c83e4d73283ee5a38c84858eec
Felipe Costa <felipeoliveiracosta2013@gmail.com>
2025-04-28 02:58:11 -0300

    Adicionando o projeto inicial, sem melhorias

---
+++ src/model/Genero.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Genero {
+	
+	public int id;
+	public String nome;
+	
+	public Genero() {
+		
+	}
+	
+	public Genero(int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+}
